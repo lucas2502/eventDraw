f8bc5e95817d6806d52df455d3cde32023f7e79f
lucas2502 <lucas2502@live.com>
2019-05-18 10:54:53 -0300

    Creando rotas em App.js

--- eventdraw/src/App.js
+++ eventdraw/src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import {BrowserRouter, Switch, Route } from 'react-dom';
 
 import Login from './pages/Login';
+import Cadastro from './pages/Cadastro';
 
 import "./styles/reset.css";
 
 class App extends Component {
     render() {
       return (
-        <>
-            <Login />
-        </>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={Login} />
+            <Route path="/Cadastro" component={Cadastro} />
+            <Route path="*" component={Login} />
+          </Switch>
+        </BrowserRouter>
       );
     }
 }
