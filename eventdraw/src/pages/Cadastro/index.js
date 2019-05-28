26931a2e4ed9dec908b8012076658e183991c0ce
lucas2502 <lucas2502@live.com>
2019-05-28 20:49:07 -0300

    Create Base folder

--- eventdraw/src/pages/Cadastro/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Cadastro = () => (
-    <h1>Pagina de Cadastro</h1>
-)
-
-export default Cadastro;
--- eventdraw/src/styles/base/index.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from 'styled-components';
-
-const Base = createGlobalStyle `
-    body {
-        font-family: 'Roboto Slab', serif;
-    }
-`
-
-export default Base;
